4f34a2d4e39cb6a3fba6d511a14032cb485d9754
logistics <info@sofane.nl>
2024-06-02 21:39:42 +0200

    19. Outputting Dynamic Values: the name

--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,7 +1,11 @@
+const names = ["Niek", "Therese"]
+
 function Post() {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1]
+
   return (
     <>
-      <p>Niek</p>
+      <p>{chosenName}</p>
       <p>This is the post.</p>
     </>
   )
