3278cb99b2716f5621db83a1291ac26e84afc8e1
logistics <info@sofane.nl>
2024-06-03 21:23:21 +0200

    24. Preparing the App for State Management: Adding Newpost.jsx

--- client/src/components/PostsList.jsx
+++ client/src/components/PostsList.jsx
@@ -1,12 +1,16 @@
 import Post from "./Post"
+import NewPost from "./NewPost"
 import classes from "./PostsList.module.css"
 
 function PostsList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Niek" body="This is post one." />
-      <Post author="Thérèse" body="This is post two." />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Niek" body="This is post one." />
+        <Post author="Thérèse" body="This is post two." />
+      </ul>
+    </>
   )
 }
 export default PostsList
