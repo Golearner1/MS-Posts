4a387ebce33c220a7df88e3f41fb565436f09460
logistics <info@sofane.nl>
2024-06-04 01:16:13 +0200

    25. Adding Event Listeners: in NewPost add function changeBodyHandler for prop onChange

--- client/src/components/NewPost.jsx
+++ client/src/components/NewPost.jsx
@@ -1,6 +1,10 @@
 import classes from "./NewPost.module.css"
 
 function NewPost() {
+  function changeBodyHandler(event) {
+    console.log(event.target.value)
+  }
+
   return (
     <form className={classes.form}>
       <p>
@@ -9,7 +13,7 @@ function NewPost() {
       </p>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
     </form>
   )
