839d920862cdf970f89d2ab8c2f28a4260b5ed8a
logistics <info@sofane.nl>
2024-06-07 01:59:28 +0200

    32. Handling Form Submission: Print a new post via NewPost to the console

--- client/src/components/PostsList.jsx
+++ client/src/components/PostsList.jsx
@@ -1,35 +1,17 @@
-import { useState } from "react"
-
 import Post from "./Post"
 import NewPost from "./NewPost"
 import Modal from "./Modal"
 import classes from "./PostsList.module.css"
 
 function PostsList({ isPosting, onStopPosting }) {
-  const [enteredBody, setEnteredBody] = useState("")
-  const [enteredAuthor, setEnteredAuthor] = useState("")
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value)
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value)
-  }
-
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onAuthorChange={authorChangeHandler}
-            onBodyChange={bodyChangeHandler}
-            onCancel={onStopPosting}
-          />
+          <NewPost onCancel={onStopPosting} />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
         <Post author="Thérèse" body="This is post two." />
       </ul>
     </>
